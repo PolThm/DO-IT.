cf6b684bfdf20b5661a7ba13ecd12b613d6c62e8
PolThm <pol.thomas56@gmail.com>
2020-08-29 14:49:26 +0200

    Title changed

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,8 +3,8 @@ import React from 'react';
 const Header: React.FC = () => {
   return (
     <header className="">
-      <h1 className="text-6xl pt-20 pb-4 text-center text-blue-500 uppercase tracking-wider font-bold">Do it.</h1>
-      <h2 className="text-3xl text-center text-blue-300">Todo List <span className="text-gray-700">by Pol Thomas</span></h2>
+      <h1 className="title text-6xl pt-16 pb-4 text-center text-blue-600 uppercase tracking-wider font-bold">Do it.</h1>
+      <h2 className="text-3xl pb-16 text-center text-blue-300">Todo List <span className="text-gray-700">by Pol Thomas</span></h2>
     </header>
   );
 };
