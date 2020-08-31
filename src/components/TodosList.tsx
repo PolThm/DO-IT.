4cfb2c8ad7f8a0163e08f589cbbce62a6f4e2c65
PolThm <pol.thomas56@gmail.com>
2020-08-31 14:22:32 +0200

    Add the ".map" function to the TodoList component

--- src/components/TodosList.tsx
+++ src/components/TodosList.tsx
@@ -1,28 +1,28 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Todo from "./Todo";
 
-let todosData = [
-  {
-    task: "Write my first Todo"
-  },
-  {
-    task: "Try the features"
-  },
-  {
-    task: "Remove the completed Todos"
-  },
-];
-
-let listOfTodos = todosData.map((todo) => {
-  return (
-    <Todo
-      key={todo.task}
-      task={todo.task}
-    />
-  )
-});
-
 const TodosList: React.FC = () => {
+  const [todosData, setTodosData] = useState([
+    {
+      task: "Write my first Todo"
+    },
+    {
+      task: "Try the features"
+    },
+    {
+      task: "Remove the completed Todos"
+    },
+  ]);
+
+  let listOfTodos = todosData.map((todo) => {
+    return (
+      <Todo
+        key={todo.task}
+        task={todo.task}
+      />
+    )
+  });
+
   return (
     <div className="">
       {listOfTodos}
