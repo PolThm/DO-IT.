8e14c8561b9b925676ac53ca73bb9e007accf0fe
PolThm <pol.thomas56@gmail.com>
2020-09-01 15:28:32 +0200

    Filters features on progress !

--- src/components/TodosFooter.tsx
+++ src/components/TodosFooter.tsx
@@ -3,9 +3,9 @@ import {DoItContext} from "../context";
 
 const TodosFooter: React.FC = () => {
   const doItContext = useContext(DoItContext);
-  const {todosAll}: any = doItContext;
+  const {todosAll, filter, changeFilter}: any = doItContext;
 
-  const [filter, setFilter] = useState("All");
+  // const [filter, setFilter] = useState("All");
 
   const baseButtonFilterClassName = "mx-1 px-2 py-1 hidden sm:block cursor-pointer border rounded border-transparent hover:border-gray-300 focus:outline-none";
   const selectedButtonFilterClassName = `${baseButtonFilterClassName} border-gray-700 hover:border-gray-700`;
@@ -24,9 +24,9 @@ const TodosFooter: React.FC = () => {
     <div className="py-2 px-4 flex items-center justify-between w-full border-solid border border-gray-700 border-t-0 text-sm">
       <p className="pr-3">{todosAll.length} items left</p>
       <div className="flex">
-        <button className={allButtonFilterClassName} onClick={() => setFilter("All")}>All</button>
-        <button className={activeButtonFilterClassName} onClick={() => setFilter("Active")}>Active</button>
-        <button className={completedButtonFilterClassName} onClick={() => setFilter("Completed")}>Completed</button>
+        <button className={allButtonFilterClassName} onClick={() => changeFilter("All")}>All</button>
+        <button className={activeButtonFilterClassName} onClick={() => changeFilter("Active")}>Active</button>
+        <button className={completedButtonFilterClassName} onClick={() => changeFilter("Completed")}>Completed</button>
       </div>
       <button className="cursor-pointer focus:outline-none hover:underline">Clear completed</button>
     </div>
