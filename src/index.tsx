1793143b4fa88bfd7d5be5e17e44162b8cdc9e0d
polth <pol.thomas56@gmail.com>
2020-11-20 14:23:59 +0100

    Remove the old Modal settings

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './tailwind.css';
 import App from "./components/App";
-import { ModalProvider } from "react-modal-hook";
-import { TransitionGroup } from "react-transition-group";
 
 ReactDOM.render(
-  <ModalProvider rootComponent={TransitionGroup}>
+  <React.StrictMode>
     <App />
-  </ModalProvider>,
+  </React.StrictMode>,
   document.getElementById('root')
 );
